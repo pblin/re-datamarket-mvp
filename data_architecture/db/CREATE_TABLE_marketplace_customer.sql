7cc29eb2733ff5c3ab92731225832e4950dcb4de
Lin, Bernard <bernardlin@outlook.com>
2018-12-04 16:02:20 -0500

    update org and customer tables

--- data_architecture/db/CREATE_TABLE_marketplace_customer.sql
+++ data_architecture/db/CREATE_TABLE_marketplace_customer.sql
@@ -9,15 +9,17 @@ CREATE TABLE marketplace.customer
     id serial NOT NULL,
     primary_email character varying(80),
     secondary_email character varying(80),
-    org_id integer REFERENCES marketplace.organization (id),
+    org_id integer,
     wallet_key_1 character varying(22),
     wallet_key_2 character varying(22),
     roles "char"[],
     last_name character varying(40),
     first_name character varying(40),
+    is_org_admin boolean, 
     CONSTRAINT customer_pkey PRIMARY KEY (id)
 )
 WITH (
     OIDS = FALSE
 )
+
 TABLESPACE pg_default;
