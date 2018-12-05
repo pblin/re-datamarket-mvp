7b0f5e31e7ebdb75831e993c66d1cbda177b0a30
Lin, Bernard <bernardlin@outlook.com>
2018-12-04 21:32:05 -0500

    corrected customer/org relationship

--- data_architecture/db/CREATE_TABLE_marketplace_customer.sql
+++ data_architecture/db/CREATE_TABLE_marketplace_customer.sql
@@ -9,7 +9,6 @@ CREATE TABLE marketplace.customer
     id serial NOT NULL,
     primary_email character varying(80),
     secondary_email character varying(80),
-    org_id integer,
     wallet_key_1 character varying(22),
     wallet_key_2 character varying(22),
     roles "char"[],
