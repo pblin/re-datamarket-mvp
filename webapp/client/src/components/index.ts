29fcc4a20183b99e5f4cc791e69ba3e3c6b93ec7
Lin, Bernard <bernardlin@outlook.com>
2019-01-29 17:24:05 -0500

    correct component name  avatar

--- webapp/client/src/components/index.ts
+++ webapp/client/src/components/index.ts
@@ -1,8 +1,7 @@
 import App from './App/App';
 import Callback from './Callback/Callback';
-import ProfileAvator from './Profile/ProfileAvator';
 import Customer from './Profile/Customer'; 
 import DatasetList from './Dashboard/DatasetList';
 import DashboardPage from './Dashboard/Dashbard';
 
-export { App, Callback, DashboardPage, ProfileAvator, Customer, DatasetList };
+export { App, Callback, DashboardPage, Customer, DatasetList };
