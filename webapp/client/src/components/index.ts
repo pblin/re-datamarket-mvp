ef30d62670327cb391ec1fa0a0f994a96ba97b2a
Lin, Bernard <bernardlin@outlook.com>
2019-01-30 19:36:55 -0500

    bug fix

--- webapp/client/src/components/index.ts
+++ webapp/client/src/components/index.ts
@@ -1,7 +1,7 @@
 import App from './App/App';
 import Customer from './Profile/Customer'; 
 import DatasetList from './Dashboard/DatasetList';
-import DashboardPage from './Dashboard/Dashbard';
+import DashboardPage from './Dashboard/Dashboard';
 import Callback from './Callback/Callback';
 
 export { App, Callback, DashboardPage, Customer, DatasetList };
