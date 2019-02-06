e36a571c505b4dae1237060582373b07772bc488
David Spires <rr505398@TX0053AM033953.prscoad.com>
2019-02-06 14:59:45 -0600

    feat(dataset-manager): made progress on the basic info form

--- webapp/client/src/components/DatasetManager/DatasetWizard/BasicInfoForm.tsx
+++ webapp/client/src/components/DatasetManager/DatasetWizard/BasicInfoForm.tsx
@@ -7,15 +7,7 @@ interface BasicFormProps {
   handleSubmit: any;
 }
 
-const renderTextField = ({input, label, meta, custom}) => {
-  console.log(input);
-  console.log(label);
-  console.log('custom');
-  console.log(custom);
-  console.log('error');
-  console.log(meta);
-  //TODO: add autofocus
-  //TODO: HelperText during an error
+const renderSelectField = ({input, label, meta, custom}) => {
   let helperText = meta.error && meta.touched ? meta.error: custom.helperText;
 
   return (
@@ -25,8 +17,33 @@ const renderTextField = ({input, label, meta, custom}) => {
         margin="normal"
         variant={'outlined'}
         label={label}
-        error={meta.touched && meta.error}
+        select
+        error={meta.dirty && meta.error}
         {...input}
+        fullWidth
+        helperText={helperText}
+      >
+        {custom.options.map(option => {
+          return (<option key={option} value={option}>{option}</option>)
+        })}
+      </TextField>
+    </Grid>
+  )
+};
+
+const renderTextField = ({input, label, meta, custom}) => {
+  let helperText = meta.error && meta.touched ? meta.error: custom.helperText;
+  console.log(input);
+  return (
+    <Grid item xs={custom.gridXs} sm={custom.gridSm}>
+      <TextField
+        className="basic-form-input"
+        margin="normal"
+        variant={'outlined'}
+        label={label}
+        error={meta.dirty && meta.error}
+        {...input}
+        type={custom.type || 'text'}
         fullWidth
         helperText={helperText}
       />
@@ -57,7 +74,68 @@ class BasicInfoForm extends Component<BasicFormProps> {
               component={renderTextField}
               name="description"
               type="text"
-              custom={ {helperText: "Description", gridXs: '12', gridSm: '6'} }
+              custom={ {helperText: "Description", gridXs: 12, gridSm: 5} }
+            />
+            <Field
+              label="Search Terms"
+              component={renderTextField}
+              name="searchTerms"
+              type="text"
+              custom={ {helperText: "Search Terms", gridXs: 12, gridSm: 3} }
+            />
+            <Field
+              label="State"
+              component={renderSelectField}
+              name="state"
+              custom={ {helperText: "State", gridXs: 12, gridSm: 2, options: ['New York']} }
+            />
+            <Field
+              label="Country"
+              component={renderSelectField}
+              name="country"
+              custom={ {helperText: "Country", gridXs: 12, gridSm: 2, options: ['Country']} }
+            />
+            <Field
+              label="Sample Api Key"
+              component={renderTextField}
+              name="sampleAPIKey"
+              type="text"
+              custom={ {helperText: "sampleAPIKey", gridXs: 12, gridSm: 6} }
+            />
+            <Field
+              label="Endpoint"
+              component={renderTextField}
+              name="endpoint"
+              type="text"
+              custom={ {helperText: "endpoint", gridXs: 12, gridSm: 6} }
+            />
+            <Field
+              label="Sample Data Key"
+              component={renderTextField}
+              name="sampleDataKey"
+              type="text"
+              custom={ {helperText: "sampleDataKey", gridXs: 12, gridSm: 6} }
+            />
+            <Field
+              label="# of records"
+              component={renderTextField}
+              name="records"
+              type="number"
+              custom={ {helperText: "records", gridXs: 12, gridSm: 2, type: 'number'} }
+            />
+            <Field
+              label="Ask Price (Low)"
+              component={renderTextField}
+              name="askPriceLow"
+              type="number"
+              custom={ {helperText: "Ask Price (Low)", gridXs: 12, gridSm: 2, type: 'number'} }
+            />
+            <Field
+              label="Ask Price (High)"
+              component={renderTextField}
+              name="askPriceHigh"
+              type="number"
+              custom={ {helperText: "Ask Price (High)", gridXs: 12, gridSm: 2, type: 'number'} }
             />
           </Grid>
           <button type="submit">Submit</button>
