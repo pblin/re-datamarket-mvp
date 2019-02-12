f86d06728891f3a723849a1242a79a25be56ba9f
David Spires <rr505398@TX0053AM033953.prscoad.com>
2019-02-12 13:00:40 -0600

    feat(appbar): improved the functionality of the app bar

--- webapp/client/src/components/App/AppLink.ts
+++ webapp/client/src/components/App/AppLink.ts
@@ -1,9 +1,11 @@
 export class AppLink {
   title: string;
   url: string;
+  icon: any; //TODO: set to JSX element
 
-  constructor(title: string, url: string) {
+  constructor(title: string, url: string, icon: any = undefined) {
     this.title = title;
     this.url = url;
+    this.icon = icon;
   }
 }
