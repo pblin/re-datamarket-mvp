e18194434c9edf30e6beed6c2cdb310ce46de4d6
David Spires <david.spires@aurea.com>
2019-02-18 18:14:35 -0600

    feat(profile): updated profile form

--- webapp/client/src/components/Profile/Customer.tsx
+++ webapp/client/src/components/Profile/Customer.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import PropTypes from 'prop-types';
-import TextField from '@material-ui/core/TextField';
 import { Auth0Authentication } from '../../auth/Auth0Authentication';
 import { Theme, withStyles, createStyles} from '@material-ui/core/styles';
 import { Redirect } from 'auth0-js';
@@ -8,13 +7,14 @@ import App from '../../components/App/App';
 import 'graphql-request';
 import { GraphQLClient } from 'graphql-request';
 import { APIKEY, GRAPHQL } from '../ConfigEnv';
-import Grid from '@material-ui/core/Grid';
+import { submit } from 'redux-form';
 import Button from '@material-ui/core/Button';
 import SaveIcon from '@material-ui/icons/Save';
-import autobind from 'autobind-decorator';
 import classNames from 'classnames';
-
-
+import {connect} from "react-redux";
+import CustomerForm from "./CustomerForm";
+import {getProfile} from "../../store/profile/profileActions";
+import {profileSelector} from "../../store/profile/profileSelector";
 
 // @ts-ignore
 const styles = (theme: Theme ) => createStyles ({
@@ -50,6 +50,9 @@ interface Props {
   theme?: any;
   // findACustomer: Function; 
   auth: Auth0Authentication;
+  getProfile: any;
+  profile: any;
+  submitProfileForm: any;
 }
 
 class Customer extends React.Component<Props> {
@@ -78,41 +81,6 @@ class Customer extends React.Component<Props> {
     });
   };
 
-  @autobind
-    async findUser () { 
-
-        const query =  `
-        query customer ($email: String ) {
-            marketplace_customer (where:{primary_email:{ _eq : $email }})
-            {
-                id
-                primary_email
-                secondary_email
-                first_name
-                last_name
-                phone
-                address
-                is_org_admin
-            }
-        }
-        `;
-        // @ts-ignore
-        const client = new GraphQLClient (GRAPHQL, {
-            headers: {
-            'X-Hasura-Access-Key': APIKEY,
-            },
-        });
-
-        let userEmail = localStorage.getItem('email');
-        const variables = {
-            email: userEmail
-        };
-        // @ts-ignore
-        let result = await client.request (query, variables);
-        // @ts-ignore
-        localStorage.setItem ('profile', JSON.stringify(result.marketplace_customer[0]));
-        this.forceUpdate();
-    } 
   // @ts-ignore
   _createCustomer = async () => {
     const mut = 
@@ -171,41 +139,17 @@ class Customer extends React.Component<Props> {
       isButtonDisabled: false
     });
 
-  }
+  };
+
 
   componentDidMount() {
-    const { authenticated } = this.props.auth;
-    if ( authenticated) {
-
-      this.findUser();
-      let email = localStorage.getItem('email');
-      this.state.primaryEmail = email;
-      let profile = localStorage.getItem('profile');
-      let profileObj = {
-        id: -1,
-        first_name: '',
-        last_name: '',
-        primary_email: email,
-        secondary_email: '',
-        phone: '',
-        address: '',
-        is_org_admin: false
-      };
-      if (profile !== 'undefined' && profile !== null ) {
-        profileObj = JSON.parse(profile);
-        this.setState ( 
-            { firstName: profileObj.first_name,
-              lastName: profileObj.last_name,
-              primaryEmail: profileObj.primary_email,
-              secondaryEmail: profileObj.secondary_email,
-              phone: profileObj.phone,
-              address: profileObj.address,
-              isOrgAdmin: profileObj.is_org_admin
-            }
-        );
-        // console.log(profileObj);
-      }
+    if(!this.props.profile) {
+      this.props.getProfile();
     }
+  }
+
+  handleProfileSubmit() {
+    console.log('Handling profile submit');
   }
 
   render() {
@@ -216,89 +160,12 @@ class Customer extends React.Component<Props> {
 
         <div> 
           <App auth={this.props.auth} />
-          <form className={classes.container} noValidate autoComplete="off">
-            <div>
-              <Grid container spacing={24}>
-                <Grid item xs={6}>
-                  <TextField 
-                      id="outlined-name"
-                      label="First Name (Required)"
-                      className={classes.textField}
-                      value={this.state.firstName}
-                      onChange={this.handleChange('firstName')}
-                      margin="normal"
-                      fullWidth
-                      variant="outlined"
-                  />
-                </Grid>
-                <Grid item  xs={6}>
-                  <TextField
-                    id="outlined-name"
-                    label="Last Name (Required)"
-                    className={classes.textField}
-                    value={this.state.lastName}
-                    onChange={this.handleChange('lastName')}
-                    margin="normal"
-                    fullWidth
-                    variant="outlined"
-                  />
-                </Grid>
-                <Grid item xs={3}>
-                  <TextField
-                    id="outlined-disabled"
-                    label="Primary Email"
-                    className={classes.textField}
-                    value={this.state.primaryEmail}
-                    margin="normal"
-                    disabled
-                    fullWidth
-                    variant="outlined"
-                  />
-                </Grid>
-                <Grid item xs={3}>
-                  <TextField
-                    id="outlined-name"
-                    label="Secondary Email"
-                    className={classes.textField}
-                    value={this.state.secondaryEmail}
-                    onChange={this.handleChange('secondaryEmail')}
-                    margin="normal"
-                    fullWidth
-                    variant="outlined"
-                  />
-                </Grid>
-                <Grid item xs={3}>
-                  <TextField
-                    id="outlined-name"
-                    label="Phone"
-                    className={classes.textField}
-                    value={this.state.phone}
-                    onChange={this.handleChange('phone')}
-                    margin="normal"
-                    fullWidth
-                    variant="outlined"
-                  />
-                </Grid>
-                <Grid item xs={12}>
-                  <TextField
-                    id="outlined-name"
-                    label="Full Address"
-                    className={classes.textField}
-                    value={this.state.address}
-                    onChange={this.handleChange('address')}
-                    margin="normal"
-                    variant="outlined"
-                    fullWidth
-                  />
-                </Grid>
-              </Grid>
-            </div>
-          </form>
-          <Button color="primary" className={classes.button} 
-                  onClick={() => this._createCustomer()} 
+          <CustomerForm onSubmit={this.handleProfileSubmit}/>
+          <Button color="primary" className={classes.button}
+                  onClick={this.props.submitProfileForm}
                   disabled={this.state.isButtonDisabled}>
-               <SaveIcon className={classNames(classes.leftIcon, classes.iconLarge)} />
-                       Save
+              <SaveIcon className={classNames(classes.leftIcon, classes.iconLarge)} />
+              Save
           </Button>
         </div>
       );
@@ -309,4 +176,17 @@ class Customer extends React.Component<Props> {
   }
 }
 
-export default withStyles ( styles, { withTheme: true } ) (Customer);
+function mapStateToProps(state: any, ownProps: any) {
+  return {
+    profile: profileSelector(state)
+  }
+}
+
+function mapDispatchToProps(dispatch: any) {
+  return {
+    getProfile: () => dispatch(getProfile()),
+    submitProfileForm: () => dispatch(submit('profile')),
+  };
+}
+
+export default withStyles ( styles, { withTheme: true } ) (connect(mapStateToProps, mapDispatchToProps)(Customer));
---
+++ webapp/client/src/components/Profile/CustomerForm.tsx
@@ -0,0 +1,137 @@
+import React, {Component} from 'react';
+import {Field, reduxForm} from 'redux-form';
+import {Grid, TextField} from "@material-ui/core";
+import {ReduxFormValidator} from "../Common/Error/ReduxFormValidator";;
+import {ERROR_TYPE} from "../Common/Error/ErrorType";
+import {connect} from "react-redux";
+import {profileSelector} from "../../store/profile/profileSelector";
+
+interface CustomerFormProps {
+   onSubmit: any;
+  //pristine: boolean;
+  //invalid: boolean;
+}
+
+const renderTextField = ({input, label, meta, custom}) => {
+  let helperText = meta.error != undefined && meta.touched ? meta.error: custom.helperText;
+
+  return (
+    <Grid item xs={custom.gridXs} sm={custom.gridSm}>
+      <TextField
+        className="basic-form-input"
+        margin="normal"
+        variant={'outlined'}
+        label={label}
+        error={meta.touched && meta.error != undefined}
+        {...input}
+        type={custom.type || 'text'}
+        fullWidth
+        helperText={helperText}
+      />
+  </Grid>
+)
+};
+
+const reduxFormValidator = new ReduxFormValidator();
+
+const validate = (values) => {
+  let errors = reduxFormValidator.validate(values, [
+    {
+      fieldName: 'firstName',
+      errors: [
+        {type: ERROR_TYPE.REQUIRED}
+      ],
+      errorMessages: [
+        'First Name is required'
+      ]
+    },
+    {
+      fieldName: 'lastName',
+      errors: [
+        {type: ERROR_TYPE.REQUIRED}
+      ],
+      errorMessages: [
+        'Last Name is required'
+      ]
+    },
+  ]);
+
+  return errors;
+};
+
+class CustomerForm extends Component<CustomerFormProps> {
+  render() {
+    return (
+      <form onSubmit={this.props.onSubmit}>
+      <Grid spacing={24} container={true} >
+        <Field
+          label="First Name"
+          component={renderTextField}
+          name="firstName"
+          type="text"
+          custom={ {helperText: "First Name", gridXs: 12, gridSm: 6} }
+        />
+        <Field
+          label="Last Name"
+          component={renderTextField}
+          name="lastName"
+          type="text"
+          custom={ {helperText: "Last Name", gridXs: 12, gridSm: 6} }
+        />
+        <Field
+          label="Primary Email"
+          component={renderTextField}
+          name="primaryEmail"
+          type="text"
+          custom={ {helperText: "Last Name", gridXs: 12, gridSm: 4} }
+        />
+        <Field
+          label="Secondary Email"
+          component={renderTextField}
+          name="secondaryEmail"
+          type="text"
+          custom={ {helperText: "Secondary Email", gridXs: 12, gridSm: 4} }
+        />
+        <Field
+          label="Phone"
+          component={renderTextField}
+          name="phone"
+          type="text"
+          custom={ {helperText: "Phone", gridXs: 12, gridSm: 4} }
+        />
+        <Field
+          label="Full Address"
+          component={renderTextField}
+          name="address"
+          type="text"
+          custom={ {helperText: "Address", gridXs: 12, gridSm: 12} }
+        />
+      </Grid>
+    </form>
+  );
+  }
+}
+
+function mapStateToProps(state) {
+    let profile = profileSelector(state);
+
+    if(!profile) {
+      return {};
+    }
+
+    console.log(profile);
+    return {
+      initialValues: {
+        firstName: profile['first_name'],
+        lastName: profile['last_name'],
+        primaryEmail: profile['primary_email'],
+        secondaryEmail: profile['secondary_email'],
+        phone: profile['phone'],
+        address: profile['address']
+      }
+    }
+}
+
+//@ts-ignore
+CustomerForm = reduxForm({form: 'profile', validate, destroyOnUnmount: false})(CustomerForm);
+export default connect(mapStateToProps)(CustomerForm);
