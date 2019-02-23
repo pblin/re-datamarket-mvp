85cd281fc30990dccb1364713f2dbfa0332d67db
David Spires <david.spires@aurea.com>
2019-02-23 14:31:29 -0600

    feat(marketplace): responsive toolbar

--- webapp/client/src/components/Marketplace/MarketplaceV2.tsx
+++ webapp/client/src/components/Marketplace/MarketplaceV2.tsx
@@ -1,10 +1,12 @@
 import * as React from "react";
 import {connect} from "react-redux";
-import {Toolbar, Button} from "@material-ui/core";
+import {Toolbar, Button, Hidden, FormControl, Select, MenuItem} from "@material-ui/core";
 import {withRouter} from "react-router";
 import './marketplace.css';
 
-interface ComponentProps {}
+interface ComponentProps {
+  schemaFilter: boolean
+}
 
 class MarketplaceV2 extends React.Component<ComponentProps> {
   constructor(props: any) {
@@ -16,8 +18,18 @@ class MarketplaceV2 extends React.Component<ComponentProps> {
     return (
       <div>
         <Toolbar className="marketplace-toolbar">
-          <Button>ALL</Button>
-          <Button><strong>OWNED BY ME</strong></Button>
+          <Hidden xsDown>
+            <Button>ALL</Button>
+            <Button><strong>OWNED BY ME</strong></Button>
+          </Hidden>
+          <Hidden smUp>
+            <FormControl>
+              <Select value={this.props.schemaFilter}>
+                <MenuItem value={'all'}>All</MenuItem>
+                <MenuItem value={'ownedByMe'}>Owned By Me</MenuItem>
+              </Select>
+            </FormControl>
+          </Hidden>
         </Toolbar>
       </div>
     )
@@ -25,7 +37,10 @@ class MarketplaceV2 extends React.Component<ComponentProps> {
 }
 
 function mapStateToProps(state: any, ownProps: any) {
+  console.log('Marketplace State');
+  console.log(state.MarketplaceState);
   return {
+    schemaFilter: state.MarketplaceState.schemaFilter
   }
 }
 
