74cb98ecae27bb88d1fd1e829d1ac2a5ffeb2df8
Lin, Bernard <bernardlin@outlook.com>
2019-02-26 15:55:15 -0500

    data related changes

--- data_architecture/db/utils/createJSONSchema.py
+++ data_architecture/db/utils/createJSONSchema.py
@@ -4,17 +4,18 @@ import json
 
 loc = ("/Users/bernardlin/Downloads/Cherre_Data_Dictionary_Master.xlsx")
 
-schemaJson =open ("nyc_lot.json", "w")
+schemaJson =open ("nyc_building.json", "w")
 
 wb = xlrd.open_workbook(loc)
-sheet = wb.sheet_by_index(7)
+sheet = wb.sheet_by_index(1)
 print ("total rows -> " + str(sheet.nrows) )
 
 fieldList = []
 for i in range (1,sheet.nrows):
-    field = {"name": "", "type": "", "description": ""};
+    field = {"name": "", "type": "", "label":"","description": ""};
     if str(sheet.cell_value(i, 1)).find("_id") == -1:
         field["name"] = sheet.cell_value(i, 1)
+        field["label"] = sheet.cell_value(i, 2)
         if sheet.cell_value(i, 0) != "USER-DEFINED" :
             field["type"] = sheet.cell_value(i, 0)
         else:
