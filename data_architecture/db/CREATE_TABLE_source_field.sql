0bbbc27f1c4747ee79411946732c25f3671d459a
Lin, Bernard <bernardlin@outlook.com>
2019-02-28 18:39:07 -0500

    udpate db scripts

--- data_architecture/db/CREATE_TABLE_source_field.sql
+++ data_architecture/db/CREATE_TABLE_source_field.sql
@@ -9,6 +9,9 @@ CREATE TABLE marketplace.source_of_field
     description text COLLATE pg_catalog."default",
     field_name text COLLATE pg_catalog."default" NOT NULL,
     field_label text COLLATE pg_catalog."default",
+    region text,
+    country text,
+    seach_terms text [],
     CONSTRAINT source_of_field_pkey PRIMARY KEY (source_id, field_name)
 )
 WITH (
@@ -31,3 +34,7 @@ CREATE INDEX fki_source_id_fkey
     ON marketplace.source_of_field USING btree
     (source_id COLLATE pg_catalog."default")
     TABLESPACE pg_default;
+
+
+-- drop index marketplace.field_label_idx
+create index field_label_idx on marketplace.source_of_field(field_label)
--- data_architecture/db/CREATE_VEW_field_in_table.sql
+++ data_architecture/db/CREATE_VEW_field_in_table.sql
@@ -5,11 +5,3 @@ SELECT column_name as field_name, table_name
 FROM information_schema.columns
 WHERE table_schema = 'cherre_sample_data'
 and column_name in (select name from marketplace.available_fields);
-
--- DROP VIEW marketplace.available_fields
-CREATE VIEW marketplace.available_fields AS
-SELECT field.name,
-    field.label,
-    field.type
-   FROM marketplace.field
-  WHERE field.is_tradable = true;
