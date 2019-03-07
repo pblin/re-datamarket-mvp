5ed1970112960e32bc692467faf75fa156f3ad8e
David Spires <david.spires@aurea.com>
2019-03-06 22:39:13 -0600

    if no schemas, message will be displayed to user to create a new schema

--- webapp/client/src/components/Marketplace/UserDatasetList.tsx
+++ webapp/client/src/components/Marketplace/UserDatasetList.tsx
@@ -8,33 +8,48 @@ import {
 
 import EditIcon from "@material-ui/icons/Edit";
 import TrashIcon from "@material-ui/icons/Delete";
+import JumboPaper from "../Common/jumboPaper";
 
 //TODO: Add more details
 //TODO: refactor schemas to be datasets
-const UserDatasetList = ({schemas, onEditClick, onDeleteClick}) => {
+const UserDatasetList = ({schemas, onEditClick, onDeleteClick, onAddClicked}) => {
+  const renderSchemaList = () => {
+    return <div>{schemas.map((schema, index) => (
+      <ExpansionPanel key={`userSchema${index}`} expanded={false}>
+        <ExpansionPanelSummary className={"schema-list"}>
+          <Grid container={true} justify={"flex-start"} className={"no-pad-right"}>
+            <Grid item xs={6} sm={9}>
+              <div className={"fake-image-own"}>
+                <p>50 x 50</p>
+              </div>
+              <p className={"header"}>{schema.name}</p>
+              <p className={"sub-header"}>{schema.description}</p>
+            </Grid>
+            <Grid item xs={6} sm={3} className={"action-container"}>
+              <Grid container justify={"flex-end"}>
+                <IconButton onClick={() =>{onEditClick(schema)}}> <EditIcon/></IconButton>
+                <IconButton onClick={()=>{onDeleteClick(schema)}}> <TrashIcon/></IconButton>
+              </Grid>
+            </Grid>
+          </Grid>
+        </ExpansionPanelSummary>
+      </ExpansionPanel>
+    ))}</div>
+  };
+
+  const renderJumboPaper = () => {
+    return  <JumboPaper
+      title={"No Datasets found"}
+      content={"Create a new Dataset by clicking on the button below"}
+      buttonText={"Create Dataset"}
+      handleClick={() => {onAddClicked()}}
+    />
+  };
+
   return(
     <div className={"schema-list"}>
-      {schemas.map((schema, index) => (
-          <ExpansionPanel key={`userSchema${index}`} expanded={false}>
-            <ExpansionPanelSummary className={"schema-list"}>
-              <Grid container={true} justify={"flex-start"} className={"no-pad-right"}>
-                <Grid item xs={6} sm={9}>
-                  <div className={"fake-image-own"}>
-                    <p>50 x 50</p>
-                  </div>
-                  <p className={"header"}>{schema.name}</p>
-                  <p className={"sub-header"}>{schema.description}</p>
-                </Grid>
-                <Grid item xs={6} sm={3} className={"action-container"}>
-                  <Grid container justify={"flex-end"}>
-                    <IconButton onClick={() =>{onEditClick(schema)}}> <EditIcon/></IconButton>
-                    <IconButton onClick={()=>{onDeleteClick(schema)}}> <TrashIcon/></IconButton>
-                  </Grid>
-                </Grid>
-              </Grid>
-            </ExpansionPanelSummary>
-        </ExpansionPanel>
-      ))}
+      {schemas.length != 0 && renderSchemaList()}
+      {schemas.length == 0 && renderJumboPaper()}
     </div>
   );
 };
