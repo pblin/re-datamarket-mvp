494fa3e1232e482951e3584e983475095a1208c5
David Spires <david.spires@aurea.com>
2019-03-13 08:12:10 -0500

    feat(dataset): turned schemalist into a smart component

--- webapp/client/src/components/DatasetManager/SchemaList/SchemaList.tsx
+++ webapp/client/src/components/DatasetManager/SchemaList/SchemaList.tsx
@@ -9,34 +9,36 @@ import {
   Typography
 } from "@material-ui/core";
 
-const SchemaList = ({schemas, onSchemaSelect}) => {
+interface SchemaProps {
+  schemas: any[];
+  onSchemaSelect: any;
+}
 
-  //TODO: Make name editable
-  //TODO: Make types editable
-  return(
-    <Paper className={"schema-dt"}>
-      <Table className={"table"}>
-        <TableHead>
-          <TableRow>
-            <TableCell> <Typography>Name</Typography> </TableCell>
-            <TableCell> <Typography>Label</Typography> </TableCell>
-            <TableCell> <Typography>Type</Typography> </TableCell>
-            <TableCell> <Typography>Description</Typography> </TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {schemas.map(schema => (
+export default class SchemaList extends React.Component<SchemaProps> {
+  render() {
+    return(
+      <Paper className={"schema-dt"}>
+        <Table className={"table"}>
+          <TableHead>
             <TableRow>
-              <TableCell align="left"><Typography>{schema.name}</Typography></TableCell>
-              <TableCell align="left"><Typography>{schema.label}</Typography></TableCell>
-              <TableCell align="left"><Typography>{schema.type}</Typography></TableCell>
-              <TableCell align="left"><Typography>{schema.description}</Typography></TableCell>
+              <TableCell> <Typography>Name</Typography> </TableCell>
+              <TableCell> <Typography>Label</Typography> </TableCell>
+              <TableCell> <Typography>Type</Typography> </TableCell>
+              <TableCell> <Typography>Description</Typography> </TableCell>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </Paper>
-  );
-};
-
-export default SchemaList;
+          </TableHead>
+          <TableBody>
+            {this.props.schemas.map(schema => (
+              <TableRow>
+                <TableCell align="left"><Typography>{schema.name}</Typography></TableCell>
+                <TableCell align="left"><Typography>{schema.label}</Typography></TableCell>
+                <TableCell align="left"><Typography>{schema.type}</Typography></TableCell>
+                <TableCell align="left"><Typography>{schema.description}</Typography></TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </Paper>
+    );
+  }
+}
