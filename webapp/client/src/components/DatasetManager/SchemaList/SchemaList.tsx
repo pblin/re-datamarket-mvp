2150e3e5c7e22022f55ad7be84655d9d1cc2a279
David Spires <david.spires@aurea.com>
2019-03-15 16:00:44 -0500

    feat(schema-table): cosmetic updates

--- webapp/client/src/components/DatasetManager/SchemaList/SchemaList.tsx
+++ webapp/client/src/components/DatasetManager/SchemaList/SchemaList.tsx
@@ -8,7 +8,8 @@ import {
   TableRow,
   TableFooter,
   TablePagination,
-  Typography
+  Typography,
+  Tooltip
 } from "@material-ui/core";
 
 interface SchemaProps {
@@ -26,11 +27,6 @@ const LabelMap = new Map();
 LabelMap.set('integer', '#c4e2fb');
 LabelMap.set('text', '#d2e9d0');
 LabelMap.set('boolean', '#f8d7da');
-
-const TypeMap = new Map();
-TypeMap.set('integer', '123');
-TypeMap.set('text', 'ABC');
-TypeMap.set('boolean', 'bool');
 
 export default class SchemaList extends React.Component<SchemaProps, SchemaState> {
   constructor(props) {
@@ -52,19 +48,15 @@ export default class SchemaList extends React.Component<SchemaProps, SchemaState
     }
 
     return (
-      <Typography style={styles}>{schema.label}</Typography>
+      <Tooltip title={schema.name}>
+        <Typography style={styles}>{schema.label}</Typography>
+      </Tooltip>
     )
   }
 
   renderTypeField(schema) {
-    let content = '';
-
-    if(TypeMap.has(schema.type)) {
-      content = TypeMap.get(schema.type);
-    }
-
     return (
-      <Typography className={"type-field"}>{content || schema.type}</Typography>
+      <Typography className={"type-field"}>{schema.type}</Typography>
     )
   }
 
@@ -83,7 +75,6 @@ export default class SchemaList extends React.Component<SchemaProps, SchemaState
           <TableHead>
             <TableRow>
               <TableCell> <Typography>Name</Typography> </TableCell>
-              <TableCell> <Typography>Label</Typography> </TableCell>
               <TableCell> <Typography>Type</Typography> </TableCell>
               <TableCell> <Typography>Description</Typography> </TableCell>
             </TableRow>
@@ -91,10 +82,11 @@ export default class SchemaList extends React.Component<SchemaProps, SchemaState
           <TableBody>
             {this.props.schemas.slice(this.state.page * this.state.rowsPerPage, this.state.page * this.state.rowsPerPage + this.state.rowsPerPage).map(schema => (
               <TableRow>
-                <TableCell align="left"><Typography className={"name-field"}>{schema.name}</Typography></TableCell>
                 <TableCell align="left">{this.renderLabelField(schema)}</TableCell>
                 <TableCell align="left">{this.renderTypeField(schema)}</TableCell>
-                <TableCell align="left"><Typography className={"description-field"}>{schema.description}</Typography></TableCell>
+                <TableCell align="left">
+                  <Tooltip title={schema.description}><Typography className={"description-field"}>{schema.description}</Typography></Tooltip>
+                </TableCell>
               </TableRow>
             ))}
           </TableBody>
