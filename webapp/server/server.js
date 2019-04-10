d4e544a1575ebe52f8e49de988592d97fc7e6564
David Spires <david.spires@aurea.com>
2019-04-10 10:17:33 -0700

    proxy and https

--- webapp/server/server.js
+++ webapp/server/server.js
@@ -1,17 +1,51 @@
+const fs = require('fs');
+const https = require('https');
+const http = require('http');
 const express = require('express');
-//const favicon = require('express-favicon');
 const path = require('path');
 const port = process.env.PORT || 3000;
 const app = express();
-//app.use(favicon(__dirname + '/build/favicon.ico'));
-// the __dirname is the current directory from where the script is running
-console.log(__dirname);
+
+const SSL_KEY = process.env.SSL_KEY;
+const SSL_PEM = process.env.SSL_PEM;
+const KEY_PASS = process.env.KEY_PASS;
+const HTTPS_ON = process.env.HTTPS_ON || 'NO';
+
+//TODO: Seperate Proxy to a different file
+const proxy = require('http-proxy-middleware');
+//TODO: Add env var for server api endpoint
+//TODO: Configure secure based on proxy
+app.use(proxy('/api', {
+    target: 'https://localhost:9000/',
+    secure: false,
+    changeOrigin: true,
+    pathRewrite: {'^/api' : ''}
+}));
+
 app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, 'build')));
+
 app.get('/ping', function (req, res) {
     return res.send('pong');
 });
 app.get('/*', function (req, res) {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
-app.listen(port);
+
+//SET UP PROXY
+
+if (HTTPS_ON == 'YES') {
+    const credentials = {
+        key: fs.readFileSync(SSL_KEY),
+        cert: fs.readFileSync(SSL_PEM),
+        passphrase: KEY_PASS
+    };
+    let httpsServer = https.createServer(credentials, app);
+    httpsServer.listen(port);
+    console.info(`Rebloc mvp running on port ${port}.`);
+}
+else {
+    let httpServer = http.createServer(app);
+    httpServer.listen(port);
+    console.info(`Rebloc mvp running on port ${port}`);
+}
