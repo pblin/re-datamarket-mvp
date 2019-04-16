a9491be48634637465136a2cd292a8d4de4b2d22
Lin, Bernard <bernardlin@outlook.com>
2019-04-16 12:44:48 -0400

    update ssl cert retrieval

--- webapp/server/config/ConfigEnv.ts
+++ webapp/server/config/ConfigEnv.ts
@@ -21,14 +21,18 @@ dotenv.config({ path: configPath });
 
 export const SSL_KEY = process.env.SSL_KEY;
 export const SSL_PEM = process.env.SSL_PEM;
+export const SSL_PFX = process.env.SSL_PFX;
 export const KEY_PASS = process.env.KEY_PASS;
 export const HTTPS_ON = process.env.HTTPS_ON || 'NO';
 export const HTTP_API_URL = process.env.HTTP_API_URL;
 export const HTTPS_API_URL = process.env.HTTPS_API_URL;
 export const PORT = process.env.PORT || 3000;
 
-console.info("KEY=" + SSL_KEY);
 console.info("HTTPS=" + HTTPS_ON);
 console.info("HTTPS=" + HTTPS_API_URL);
-console.info("PEM=" + SSL_PEM);
+console.info("KEY=" + SSL_KEY);
+console.info("KEY=" + SSL_PEM);
+console.info("PEM=" + SSL_PFX);
+
+
 
