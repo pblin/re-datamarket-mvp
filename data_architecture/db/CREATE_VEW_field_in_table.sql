951c42a22760ce25ccb7ab40ceb9209dfda7cd9f
Lin, Bernard <bernardlin@outlook.com>
2019-04-24 13:37:40 -0400

    update database field

--- data_architecture/db/CREATE_VEW_field_in_table.sql
+++
@@ -1,7 +0,0 @@
-
--- DROP VIEW marketplace.field_in_table
-CREATE VIEW marketplace.field_in_table AS
-SELECT column_name as field_name, table_name
-FROM information_schema.columns
-WHERE table_schema = 'cherre_sample_data'
-and column_name in (select name from marketplace.available_fields);
