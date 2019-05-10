d861447199db4da012a3648cb4f39667f0d2b9b8
David Spires <david.spires@aurea.com>
2019-05-10 13:01:34 -0500

    should fix build

--- webapp/start_webapp.sh
+++ webapp/start_webapp.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 CONTAINER_IMAGE=$1
-if [ `docker ps | grep searchapi | wc -l` -gt 0 ]; then
+if [ `docker ps | grep reblocapp | wc -l` -gt 0 ]; then
 	docker stop reblocapp
 fi
-docker container prune -f 
+docker container prune -f
 docker run -d --rm -p 3000:3000 -e NODE_ENV=production --name=reblocapp $CONTAINER_IMAGE
