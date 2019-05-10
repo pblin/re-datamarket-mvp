0812a37ff0a338ea161ecd2d48e8036dd8f592be
David Spires <david.spires@aurea.com>
2019-05-10 12:05:15 -0500

    working sample data

--- webapp/client/src/components/Common/Table/DynamicTable.tsx
+++ webapp/client/src/components/Common/Table/DynamicTable.tsx
@@ -1,78 +1,77 @@
 import * as React from "react";
 import {
+  Button,
   Grid,
+  Paper,
   Table,
   TableBody,
   TableHead,
   TableCell,
   TableRow,
+  Toolbar,
   Typography
 } from "@material-ui/core";
 import '../common.scss';
 import Downloader from '../File/Downloader';
 
-/**
- *
- * @param data
- * TODO
- * (1) Download JSON (Done)
- * (2) Download CSV (Done)
- * (3) Style
- * (4) Provide Dynamic Table On Dataset
- * (5) Only show table if the user is not the owner of the dataset
- * (6) Provide Sample Data Download
- */
-const generateProps = (data) => {
-  const dataProps = [];
-
-  if(data.length) {
-    for(const k in data[0]) {
-      dataProps.push(k);
-    }
-  }
-
-  return dataProps;
-};
-
 const renderTableCells = (row, objProps) => {
   return <TableRow>
     {objProps.map((prop) => (
       <TableCell>
-        {row[prop] && row[prop].toString()}
+        <Typography>{row[prop] && row[prop].toString()}</Typography>
       </TableCell>
     ))}
   </TableRow>;
 };
 
+const renderToolbar = (data) => {
+  return <Toolbar className={"dynamic-toolbar"}>
+    <div className={"title-container"}>
+      <Typography variant={"subtitle1"}><strong>Sample Data</strong></Typography>
+    </div>
+    <div>
+      <Button
+        variant={"outlined"}
+        color={"secondary"}
+        onClick={() => {Downloader.downloadFile('sample.json', data)}}
+      >
+        Download JSON
+      </Button>
+      <Button
+        variant={"outlined"}
+        color={"secondary"}
+        onClick={() => {Downloader.downloadFileAsCSV('sample.csv', data)}}
+      >
+        Download CSV
+      </Button>
+    </div>
+  </Toolbar>
+};
+
 const DynamicTable = ({data}) => {
-  console.log('Here is the data passed');
-  console.log(data);
-
-  console.log(generateProps(data));
-
   const dataProps = Downloader.extractFieldsFromData(data);
 
   return(
     <Grid container={true} justify={"center"}>
-      <button onClick={() => {Downloader.downloadFile('test.json', data)}}>Click Me</button>
-      <button onClick={() => {Downloader.downloadFileAsCSV('test.csv', data)}}>Click Me CSV</button>
-      Dynamic Table
-      <Table className={"common-table"}>
-        <TableHead>
-          <TableRow>
-            <TableCell>
+      <Paper className={"scroll-paper"}>
+        {renderToolbar(data)}
+        <Table className={"common-table"}>
+          <TableHead>
+            <TableRow>
                 {dataProps.map((prop) => (
                   <TableCell><Typography>{prop}</Typography></TableCell>
                 ))}
-            </TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {data.map((row) => (
-            <React.Fragment>{renderTableCells(row, dataProps)}</React.Fragment>
-          ))}
-        </TableBody>
-      </Table>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {data.map((row) => (
+              <React.Fragment>
+                {renderTableCells(row, dataProps)}
+              </React.Fragment>
+            ))}
+          </TableBody>
+        </Table>
+      </Paper>
     </Grid>
   );
 };
