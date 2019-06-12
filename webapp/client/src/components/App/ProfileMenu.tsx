e78e8c85479274289fa58a4cd24f98b5d5135064
David Spires <david.spires@aurea.com>
2019-06-11 23:19:48 -0500

    fixes issue with profileMenu

--- webapp/client/src/components/App/ProfileMenu.tsx
+++ webapp/client/src/components/App/ProfileMenu.tsx
@@ -1,5 +1,12 @@
 import * as React from "react";
-import {Menu, MenuItem, ClickAwayListener, Paper, Divider, Typography} from "@material-ui/core";
+import {
+  Menu,
+  MenuItem,
+  MenuList,
+  ClickAwayListener,
+  Divider,
+  Typography
+} from "@material-ui/core";
 import {Link} from "react-router-dom";
 
 const ProfileMenu = ({profile, open, onClickAway}) => {
@@ -11,24 +18,27 @@ const ProfileMenu = ({profile, open, onClickAway}) => {
   };
 
   return(
-      <Paper>
-        <ClickAwayListener onClickAway={() => handleClose('clickAway')}>
           <Menu  anchorEl={document.getElementById('avatar')}
                  open={open}>
-              {(profile['first_name'] && profile['last_name']) && (
-                <div>
-                  <MenuItem><Typography>Signed in as {profile['first_name']} {profile['last_name']}</Typography></MenuItem>
-                  <Divider />
-                </div>
-              )
-              }
-              <MenuItem onClick={() => handleClose('profile')}>
-                <Typography><Link to="/profile">My Profile</Link></Typography>
-              </MenuItem>
-              <MenuItem onClick={() => handleClose('logout')}><Typography>Logout</Typography></MenuItem>
+            <ClickAwayListener onClickAway={() => handleClose('clickAway')}>
+              <MenuList>
+                {(profile['first_name'] && profile['last_name']) && (
+                  <div>
+                    <MenuItem>
+                      <Typography>Signed in as {profile['first_name']} {profile['last_name']}</Typography>
+                    </MenuItem>
+                    <Divider />
+                  </div>
+                )}
+                <MenuItem onClick={() => handleClose('profile')}>
+                  <Typography><Link to="/profile">My Profile</Link></Typography>
+                </MenuItem>
+                <MenuItem onClick={() => handleClose('logout')}>
+                  <Typography>Logout</Typography>
+                </MenuItem>
+              </MenuList>
+            </ClickAwayListener>
           </Menu>
-        </ClickAwayListener>
-      </Paper>
   );
 };
 
