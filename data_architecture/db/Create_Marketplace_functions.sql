75d05936f8432a58ae58ea586dbd2cb958b52437
Lin, Bernard <bernardlin@outlook.com>
2019-06-21 09:07:23 -0400

    update db fuctions's scripts

--- data_architecture/db/Create_Marketplace_functions.sql
+++ data_architecture/db/Create_Marketplace_functions.sql
@@ -1,25 +1,27 @@
-create function marketplace.search_dataset(topics text, terms text, cities text, region text)
+create function marketplace.search_dataset(topics text, terms text, cities text, region text, ctn text)
 returns setof marketplace.data_source_detail as $$
 
-select * from marketplace.data_source_detail 
-where ( terms = '' OR terms % ANY (search_terms) 
-		OR terms %> ( name || ' ' || description ) OR terms %> (state_province || ' ' || country) 
-		OR terms % ANY (city) )
-AND ( topics = '' OR topics % ANY (topic) )
-AND ( cities = '' OR ( cities % ANY (city)  
-					   and (region = ''  or region <% (state_province || ' ' || country ))
-					 ))
+	select * from marketplace.data_source_detail 
+	where ( terms = '' OR terms % ANY (search_terms) 
+			OR terms %> ( name || ' ' || description ) OR terms %> (state_province || ' ' || country) 
+			OR terms % ANY (city) )
+	AND ( topics = '' OR topics % ANY (topic) )
+	AND ( cities = '' OR cities % ANY (city) ) 
+	AND ( region = '' OR region <% state_province ) 
+	AND ( ctn = '' OR ctn <% country )
+
 
 $$ language sql stable;
 
-create function marketplace.search_dataset_schema (fields text, topics text, cities text, region text)
+create function marketplace.search_dataset_schema (fields text, topics text, cities text, state text, country text)
 returns setof marketplace.field_in_schema as $$
 
 	select * from marketplace.field_in_schema
 		where fields %> field_label 
 			and (topics = '' or topics % any (topic))
 			and (cities = '' or cities % any (city))
-			and (region = '' or region %> ( state_province || ' ' || country ))
+			and (state = '' or state %> (state_province))
+			and (country = '' or country %> (country))
 											  
 $$ language sql stable;
 
